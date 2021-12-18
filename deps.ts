bf6d91fc03191562ff0017bf286237de686a6550
tomholford <tomholford@users.noreply.github.com>
2021-12-18 15:46:00 -0800

    add import map and lockfile

--- deps.ts
+++ deps.ts
@@ -1,9 +1,7 @@
-import { join } from "https://deno.land/std@0.118.0/path/mod.ts";
-import { ensureDir } from "https://deno.land/std@0.118.0/fs/mod.ts";
-import Denomander from "https://deno.land/x/denomander@0.9.1/mod.ts";
+export { join } from "path";
+export { ensureDir } from "fs";
+import Denomander from "denomander";
 
 export {
-  Denomander,
-  ensureDir,
-  join,
+  Denomander
 };
